af1a9d744fcf5c22298ab551ea89ef726683209a
Ronaldo Carvalho <ronaldocarvalho798@gmail.com>
2025-03-10 13:18:39 -0300

    fix limit maxRecords

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 551
-const limit = 20
+const maxRecords = 501
+const limit = 10
 let offset = 0;
 
 function convertWeightToKg(weightInGrams) {
